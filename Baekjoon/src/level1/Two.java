156960b3b11b765cb64f6254913e620f577d0792
You-Seongjin <seongjin511@naver.com>
2023-11-12 21:30:48 +0900

    231112_Baekjoon

---
+++ Baekjoon/src/level1/Two.java
@@ -0,0 +1,19 @@
+package level1;
+
+import java.io.*;
+
+public class Two {
+
+	public static void main(String[] args) throws IOException{
+
+		int num1 = System.in.read()-'0';
+		System.in.skip(1);
+		int num2 = System.in.read()-'0';
+		
+		int sum = num1 + num2;
+		
+		System.out.print(sum);
+
+	}
+
+}

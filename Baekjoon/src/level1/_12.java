49cb077e7d13ec753ffbbc6fe3e0107002d7ce68
You-Seongjin <seongjin511@naver.com>
2023-11-22 21:36:33 +0900

    231122_Baekjoon

---
+++ Baekjoon/src/level1/_12.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class _12 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}

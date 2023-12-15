2530974c242ced31bb4a94a780bbb426518b0ddb
You-Seongjin <seongjin511@naver.com>
2023-12-15 22:13:09 +0900

    231215_Programmers

--- Programmers/src/level0/_030.java
+++ Programmers/src/level0/_030.java
@@ -1,6 +1,12 @@
 package level0;
 
 public class _030 {
+	
+	public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
